6119353fd7d17749d45869ec50b6302bb86b1a68
Filype Pereira <pereira.filype@gmail.com>
2014-12-07 13:10:46 +1300

    homepage

--- app.js
+++ app.js
@@ -110,6 +110,8 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  */
 
 app.get('/', homeController.index);
+app.get('/votd', homeController.votd);
+
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
--- controllers/home.js
+++ controllers/home.js
@@ -3,8 +3,26 @@
  * Home page.
  */
 
-exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
+var _ = require('lodash');
+var bible = require('bible-english');
+
+exports.votd = function(req, res) {
+
+  bible.getVerse('votd', function (err, data) {
+    return res.send(err || data[0]);
   });
+
+};
+
+exports.index = function(req, res) {
+
+  bible.getVerse('votd', function (err, data) {
+
+    res.render('home', {
+      title: 'Home',
+      votd : _.first(data)
+    });
+
+  });
+
 };
